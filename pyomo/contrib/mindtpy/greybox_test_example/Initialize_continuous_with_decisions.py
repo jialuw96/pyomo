baa555e75965a67178c731468a27b06083d7895c
jialuw96 <jwang44@nd.edu>
2023-11-01 22:42:06 -0400

    update warmstart functions

--- pyomo/contrib/mindtpy/greybox_test_example/Initialize_continuous_with_decisions.py
+++ pyomo/contrib/mindtpy/greybox_test_example/Initialize_continuous_with_decisions.py
@@ -67,6 +67,10 @@ def initialize(mod):
 
         
 
+    def initialize(mod):
+    
+    new_fim = np.zeros((4,4))
+    
     ### compute FIM 
     def eval_fim2(m, a, b):
         """
@@ -80,9 +84,9 @@ def initialize(mod):
             for i in m.n_responses:
                 for j in m.n_responses:
                     if j>=i:
-                        summi += m.cov_y[i,j]*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
+                        summi += m.cov_y[i,j].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
                     else:
-                        summi += m.cov_y[j,i]*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
+                        summi += m.cov_y[j,i].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
 
 
             return summi
@@ -91,44 +95,46 @@ def initialize(mod):
             for i in m.n_responses:
                 for j in m.n_responses:
                     if j>=i:
-                        summi += m.cov_y[i,j]*unit_fim_list[i*m.num_measure_dynamic_flatten+j][b][a]
+                        summi += m.cov_y[i,j].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][b][a]
                     else:
-                        summi += m.cov_y[j,i]*unit_fim_list[i*m.num_measure_dynamic_flatten+j][b][a]
+                        summi += m.cov_y[j,i].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][b][a]
 
 
             return summi
-
-        
+            
     for a in mod.DimFIM:
         for b in mod.DimFIM:
             if a<=b:
                 dynamic_initial_element = eval_fim2(mod, a,b)
-                mod.TotalFIM[a,b] = dynamic_initial_element
-               
+                mod.TotalFIM[a,b].value = dynamic_initial_element
+                new_fim[a,b] = dynamic_initial_element
+                new_fim[b,a] = dynamic_initial_element
+                
                 ### update greybox
                 grey_box_name = "ele_"+str(a)+"_"+str(b)
                 mod.my_block.egb.inputs[grey_box_name].value = dynamic_initial_element
                 
-                
+    
+    # initialize determinant 
+    _, det = np.linalg.slogdet(new_fim)
+    mod.my_block.egb.outputs["log_det"] = det 
+    
     # manual and dynamic install initial 
     def total_dynamic_exp(m):
-        return sum(m.cov_y[i,i] for i in range(m.n_static_measurements, m.num_measure_dynamic_flatten))
+        return sum(m.cov_y[i,i].value for i in range(m.n_static_measurements, m.num_measure_dynamic_flatten))
         
     total_dynamic_initial = total_dynamic_exp(mod)
-    mod.total_number_dynamic_measurements = total_dynamic_initial 
+    mod.total_number_dynamic_measurements.value = total_dynamic_initial 
       
     ### cost constraints
     def cost_compute(m):
         """Compute cost
         cost = static-cost measurement cost + dynamic-cost measurement installation cost + dynamic-cost meausrement timepoint cost 
         """
-        return sum(m.cov_y[i,i]*m.cost_list[i] for i in m.n_responses)+sum(m.if_install_dynamic[j]*m.dynamic_install_cost[j-m.n_static_measurements] for j in m.DimDynamic)
+        return sum(m.cov_y[i,i].value*m.cost_list[i] for i in m.n_responses)+sum(m.if_install_dynamic[j].value*m.dynamic_install_cost[j-m.n_static_measurements] for j in m.DimDynamic)
 
     cost_init = cost_compute(mod)
-    mod.cost = cost_init
-        
+    mod.cost.value = cost_init
+                
     return mod
-
-
-update_mod = initialize(mod)
-
+                
--- pyomo/contrib/mindtpy/greybox_test_example/warmstart_rotary_bed.py
+++ pyomo/contrib/mindtpy/greybox_test_example/warmstart_rotary_bed.py
@@ -11,6 +11,8 @@ def initialize(mod):
 
     with open("rotary_bed_unit_FIM", 'rb') as f:
         unit_fim_list = pickle.load(f)
+        
+    new_fim = np.zeros((5,5))
     
     ### compute FIM 
     def eval_fim2(m, a, b):
@@ -25,9 +27,9 @@ def initialize(mod):
             for i in m.n_responses:
                 for j in m.n_responses:
                     if j>=i:
-                        summi += m.cov_y[i,j]*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
+                        summi += m.cov_y[i,j].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
                     else:
-                        summi += m.cov_y[j,i]*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
+                        summi += m.cov_y[j,i].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][a][b]
 
 
             return summi
@@ -36,29 +38,34 @@ def initialize(mod):
             for i in m.n_responses:
                 for j in m.n_responses:
                     if j>=i:
-                        summi += m.cov_y[i,j]*calculator.fim_collection[i*m.num_measure_dynamic_flatten+j][b][a]
+                        summi += m.cov_y[i,j].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][b][a]
                     else:
-                        summi += m.cov_y[j,i]*calculator.fim_collection[i*m.num_measure_dynamic_flatten+j][b][a]
+                        summi += m.cov_y[j,i].value*unit_fim_list[i*m.num_measure_dynamic_flatten+j][b][a]
 
 
             return summi
             
-    #mod.dynamic_initial_FIM = pyo.Expression(mod.DimFIM_half, rule=eval_fim2)
-        
+ 
     for a in mod.DimFIM:
         for b in mod.DimFIM:
             if a<=b:
                 dynamic_initial_element = eval_fim2(mod, a,b)
                 mod.TotalFIM[a,b].value = dynamic_initial_element
-                #new_fim[a,b] = dynamic_initial_element
+                new_fim[a,b] = dynamic_initial_element
+                new_fim[b,a] = dynamic_initial_element
                 
                 ### update greybox
-                #grey_box_name = "ele_"+str(a)+"_"+str(b)
-                #mod.my_block.egb.inputs[grey_box_name].value = dynamic_initial_element
+                grey_box_name = "ele_"+str(a)+"_"+str(b)
+                mod.my_block.egb.inputs[grey_box_name].value = dynamic_initial_element
+                
+    
+    # initialize determinant 
+    _, det = np.linalg.slogdet(new_fim)
+    mod.my_block.egb.outputs["log_det"] = det 
           
     # manual and dynamic install initial 
     def total_dynamic_exp(m):
-        return sum(m.cov_y[i,i] for i in range(m.n_static_measurements, m.num_measure_dynamic_flatten))
+        return sum(m.cov_y[i,i].value for i in range(m.n_static_measurements, m.num_measure_dynamic_flatten))
         
     total_dynamic_initial = total_dynamic_exp(mod)
     mod.total_number_dynamic_measurements.value = total_dynamic_initial 
@@ -68,7 +75,7 @@ def initialize(mod):
         """Compute cost
         cost = static-cost measurement cost + dynamic-cost measurement installation cost + dynamic-cost meausrement timepoint cost 
         """
-        return sum(m.cov_y[i,i]*m.cost_list[i] for i in m.n_responses)+sum(m.if_install_dynamic[j]*m.dynamic_install_cost[j-m.n_static_measurements] for j in m.DimDynamic)
+        return sum(m.cov_y[i,i].value*m.cost_list[i] for i in m.n_responses)+sum(m.if_install_dynamic[j].value*m.dynamic_install_cost[j-m.n_static_measurements] for j in m.DimDynamic)
 
     cost_init = cost_compute(mod)
     mod.cost.value = cost_init
